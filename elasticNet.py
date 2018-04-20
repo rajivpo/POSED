0e8eea47c72226d410818016b183396ce8c5322c
Rajiv Patel-O'Connor <rajiv.patel.oconnor@gmail.com>
2018-04-20 13:04:58 -0400

    included test error code

--- elasticNet.py
+++ elasticNet.py
@@ -8,6 +8,9 @@ Created on Fri Apr 20 02:30:09 2018
 import numpy as np
 import math
 from sklearn.linear_model import ElasticNet
+from sklearn.preprocessing import StandardScaler
+
+scalr = StandardScaler();
 
 #load five folds
 fold1 = np.loadtxt('../projData/ObesityCVFold_1.csv', delimiter=',', skiprows =1)
@@ -18,7 +21,7 @@ fold5 = np.loadtxt('../projData/ObesityCVFold_5.csv', delimiter=',', skiprows =1
 
 
 #define parameters
-l1_ratio = np.linspace(.01, .9, 9)
+l1_ratio = [0.5, 0.6, 0.7, 0.8, 0.9]
 alpha = [.1, .5, .7, .9, .95, .99, 1]
 weights = np.zeros([len(l1_ratio), len(alpha), 5, 123])
 bias = np.zeros([len(l1_ratio), len(alpha), 5])
@@ -46,14 +49,19 @@ for f in range(0, 5):
                 
             #train algo and get weights    
             [r, c] = np.shape(test)
-            EN = ElasticNet(alpha=alpha[a], l1_ratio=l1_ratio[l], max_iter=10000)
-            EN.fit(train[:, 0:c-1], train[:, c-1])
+            X = train[:, 0:c-1]
+            y = train[:, c-1]
+            #standardize X
+            X = scalr.fit_transform(X)
+            EN = ElasticNet(alpha=alpha[a], l1_ratio=l1_ratio[l], max_iter=3000, fit_intercept=True)
+            EN.fit(X, y)
             weights[l, a, f, :] = EN.coef_
             bias[l, a, f] = EN.intercept_
             
-            #calculate MSE
-            [r, c] = np.shape(test)
-            yHat = EN.predict(test[:, 0:c-1])
+            #calculate RMSE
+            Xtest = test[:, 0:c-1]
+            Xtest = scalr.fit_transform(Xtest)
+            yHat = EN.predict(Xtest)
             RMSE[l, a, f] = math.sqrt(np.sum(np.power(yHat - test[:, c-1], 2))/r)
             
 #determine ideal combo of alpha + l1_ratio from crossval
@@ -63,7 +71,17 @@ weights = np.mean(weights, 2)
 
 #get best values from 
 [rmin, cmin] = np.unravel_index(RMSE.argmin(), np.shape(RMSE))
+RMSEmin = RMSE[rmin, cmin]
 biasMin = bias[rmin, cmin]
 weightsMin = weights[rmin, cmin, :]
 alphaMin = alpha[cmin]
-l1_ratioMin = alpha[rmin]
+l1_ratioMin = l1_ratio[rmin]
+
+
+#load testData and test with appropriate vals
+testData = np.loadtxt('../projData/ObesityTestSet.csv', delimiter=',', skiprows = 1)
+[r, c] = testData.shape
+Xtest = testData[:, 0:c - 1]
+Xtest = scalr.fit_transform(Xtest)
+yHatTest = np.matmul(Xtest, weightsMin) + biasMin
+RMSEtest = math.sqrt(np.sum(np.power(yHatTest - testData[:, c-1], 2))/r)
